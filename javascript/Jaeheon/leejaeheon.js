796e0224ee5da493c7209266ffafa3a0534e13ac
lucelin0414 <lucelin0414@gmail.com>
2022-11-15 15:29:06 +0900

    ver1.11

---
+++ javascript/Jaeheon/leejaeheon.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $(".q1").click(function(){
+        $("#a1").toggle("fast", "swing");
+    })
+})
+$(document).ready(function(){
+    $(".q2").click(function(){
+        $("#a2").toggle("fast", "swing");
+    })
+})
+$(document).ready(function(){
+    $(".q3").click(function(){
+        $("#a3").toggle("fast", "swing");
+    })
+})
+$(document).ready(function(){
+    $(".q4").click(function(){
+        $("#a4").toggle("fast", "swing");
+    })
+})
+$(document).ready(function(){
+    $(".q5").click(function(){
+        $("#a5").toggle("fast", "swing");
+    })
+})
+$(document).ready(function(){
+    $(".q6").click(function(){
+        $("#a6").toggle("fast", "swing");
+    })
+})
+$(document).ready(function(){
+    $(".q7").click(function(){
+        $("#a7").toggle("fast", "swing");
+    })
+})
+$(document).ready(function(){
+    $(".q8").click(function(){
+        $("#a8").toggle("fast", "swing");
+    })
+})
