4b49f1e05ff024d0dbe0c7cc54a664dc107e61e0
fenix302 <fenix302@naver.com>
2022-11-15 16:20:17 +0900

    jinho3

--- javascript/Jinho/jinho.js
+++ javascript/Jinho/jinho.js
@@ -1,14 +1,38 @@
+// faq script 시작
 $(document).ready(function(){
     $(".faq .q:eq(0)").click(function(){
-        $(".faq .a:eq(0)").slideToggle(100,"linear");
+        $(".faq .a_box:eq(0)").slideToggle(100,"linear");
+        $(".faq .a_box:not(:eq(0))").slideUp(100,"linear");
     })
     $(".faq .q:eq(1)").click(function(){
-        $(".faq .a:eq(1)").slideToggle(100,"linear");
+        $(".faq .a_box:eq(1)").slideToggle(100,"linear");
+        $(".faq .a_box:not(:eq(1))").slideUp(100,"linear");
+
     })
     $(".faq .q:eq(2)").click(function(){
-        $(".faq .a:eq(2)").slideToggle(100,"linear");
+        $(".faq .a_box:eq(2)").slideToggle(100,"linear");
+        $(".faq .a_box:not(:eq(2))").slideUp(100,"linear");
+
     })
     $(".faq .q:eq(3)").click(function(){
-        $(".faq .a:eq(3)").slideToggle(100,"linear");
+        $(".faq .a_box:eq(3)").slideToggle(100,"linear");
+        $(".faq .a_box:not(:eq(3))").slideUp(100,"linear");
+
+    })
+    $(".faq .q:eq(4)").click(function(){
+        $(".faq .a_box:eq(4)").slideToggle(100,"linear");
+        $(".faq .a_box:not(:eq(4))").slideUp(100,"linear");
+
+    })
+    $(".faq .q:eq(5)").click(function(){
+        $(".faq .a_box:eq(5)").slideToggle(100,"linear");
+        $(".faq .a_box:not(:eq(5))").slideUp(100,"linear");
+
+    })
+    $(".faq .q:eq(6)").click(function(){
+        $(".faq .a_box:eq(6)").slideToggle(100,"linear");
+        $(".faq .a_box:not(:eq(6))").slideUp(100,"linear");
+
     })
 })
+// faq script 종료
