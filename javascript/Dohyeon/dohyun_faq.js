ce09aaa66affa4c35971562ff9a65764256f1c3b
leedotaeng <116795024+leedotang@users.noreply.github.com>
2022-11-18 09:33:37 +0900

    dohyun.js

---
+++ javascript/Dohyeon/dohyun_faq.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.faq-box__question:eq(0)').click(function(){
+        $('.faq-box__answer:eq(0)').slideToggle('fast');
+    });
+    $('.faq-box__question:eq(1)').click(function(){
+        $('.faq-box__answer:eq(1)').slideToggle('fast');
+
+    });
+    $('.faq-box__question:eq(2)').click(function(){
+        $('.faq-box__answer:eq(2)').slideToggle('fast');
+    });
+    $('.faq-box__question:eq(3)').click(function(){
+        $('.faq-box__answer:eq(3)').slideToggle('fast');
+
+    });
+    $('.faq-box__question:eq(4)').click(function(){
+        $('.faq-box__answer:eq(4)').slideToggle('fast');
+    });
+    $('.faq-box__question:eq(5)').click(function(){
+        $('.faq-box__answer:eq(5)').slideToggle('fast');
+
+    });
+});
+    
